36c77bfa69248fb8628d0380df57f898723ffb87
Henrik Bjørlo <henrik.bjorlo@fiken.no>
2019-09-24 09:35:40 +0200

    gitpkg

---
+++ website/modules/components/MarkdownViewer.js
@@ -0,0 +1,14 @@
+/*eslint-disable react/no-danger*/
+import React from "react";
+import PropTypes from "prop-types";
+import "prismjs/themes/prism-tomorrow.css";
+
+export default function MarkdownViewer({ html, id }) {
+  return (
+    <div className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
+  );
+}
+
+MarkdownViewer.propTypes = {
+  html: PropTypes.string.isRequired
+};
